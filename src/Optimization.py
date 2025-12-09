7aee0c62216c0957983e7b38e55390750589321b
Leo-Simpson <52931376+Leo-Simpson@users.noreply.github.com>
2025-12-09 13:38:48 +0100

    Better code of the interior point method with step function defined in symoblics

--- src/Optimization.py
+++ src/Optimization.py
@@ -23,22 +23,39 @@ def InteriorPointSolver(scale, symbolics, x0, opts={}):
         using (primal-dual/log-barrier) interior point method with line-searched globalization approach and GN Hessian approximation.
 
         Note that the positive definiteness of P(x) is only checked during line-search, not enforced as a constraint, because we assume that h(x) already acts as a barrier for it.
+
+        The interior point method performs Newton steps on the relaxed KKT conditions:
+            grad f(x) - mu * grad h(x) = 0
+            mu * h(x) - tau = 0
+
+
     """
     opts = {**default_opts, **opts} # Merge default options with user options
-    J_sym = ca.jacobian( symbolics['r'], symbolics['x'])
+    
+    tau_sym = ca.SX.sym("tau")  # symbolic barrier parameter to create the casadi functions
+    mu_sym = ca.SX.sym("mu")  # symbolic dual variable for the inequality constraint to create the casadi functions
+
     value = 0.5 * scale * ca.sumsqr( symbolics['r'])
-    gradient = scale * J_sym.T @ symbolics['r']
-    hessian = scale * J_sym.T @ J_sym  + opts["L2_pen"] * ca.DM.eye(x0.shape[0])
-    sqrtP_sym = ca.trace(ca.chol(symbolics['P'])) # this is regular (i.e. not nan) iff P is positive definite
-
-    grad_h_sym = ca.jacobian(symbolics['h'], symbolics['x']).T
-    merit_sym = value - opts["tau"] * ca.log(symbolics['h'])
-
+    sqrtP = ca.trace(ca.chol(symbolics['P'])) # this is regular (i.e. not nan) iff P is positive definite
+    merit = value - tau_sym * ca.log(symbolics['h'])
     f_fn = ca.Function("f", [symbolics['x']], [value])
-    merit_fn = ca.Function("merit", [symbolics['x']], [merit_sym])
-    cheap_h = ca.Function("h", [symbolics['x']], [symbolics['h'], sqrtP_sym])
-    expensive_h = ca.Function("h_full", [symbolics['x']], [symbolics['h'], grad_h_sym])
-    expensive_f = ca.Function("grad", [ symbolics['x']], [gradient, hessian])
+    merit_fn = ca.Function("merit", [symbolics['x'], tau_sym], [merit])
+    cheap_h = ca.Function("h", [symbolics['x']], [symbolics['h'], sqrtP])
+    
+    # Now create the function that computes the step
+    J_sym = ca.jacobian( symbolics['r'], symbolics['x'])
+    gradient_f = scale * J_sym.T @ symbolics['r']
+    hessian_f = scale * J_sym.T @ J_sym  + opts["L2_pen"] * ca.DM.eye(x0.shape[0])
+    grad_h = ca.jacobian(symbolics['h'], symbolics['x']).T
+
+    # Now solve the condensed KKT system
+    grad_cond = gradient_f - (tau_sym / symbolics['h']) * grad_h
+    H_cond = hessian_f + (mu_sym / symbolics['h']) * (grad_h @ grad_h.T)
+    dx = - ca.solve(H_cond, grad_cond)
+    dmerit = grad_cond.T @ dx  # this derivative is always negative
+    dmu = (tau_sym  - mu_sym * symbolics['h'] - mu_sym * grad_h.T @ dx )/symbolics['h'] # linearizaation of tau = mu * h ==> tau = mu * h + mu * dh + dmu * h ==> dmu = (tau - mu * h - mu * dh)/h
+
+    step_fn = ca.Function("step", [ symbolics['x'], mu_sym, tau_sym], [dx, dmu, dmerit])
 
     stats = {"f_fn": f_fn, "success": False}
     x_k = x0.copy()
@@ -46,33 +63,28 @@ def InteriorPointSolver(scale, symbolics, x0, opts={}):
     iterates = [x_k]
     for k in range(1, opts["max_iters"]+1):
         tau_k = opts["tau"]  # Note that usually, to get better convergence, tau should start bigger, and decrease along iterations. Here, we fix it to make the code simpler, but the convergence will be slower...
-        hk, grad_hk = expensive_h(x_k)
-        if hk < 0:
+        hk, sqrtPk = cheap_h(x_k)
+        if not sqrtPk.is_regular() or hk < 0 or mu_k < 0:
             stats["reason"] = f"iterate is not feasible"
             break
-        grad_fk, H_fk = expensive_f(x_k) # This is in vector form
-    
         # Now compute the step
-        grad_tot_k = grad_fk - (tau_k / hk) * grad_hk # gradient of the merit function
-        H_tot = H_fk + (mu_k / hk) * (grad_hk @ grad_hk.T)  # Hessian of the merit function
-        dx_k = - ca.solve(H_tot, grad_tot_k) # Compute the step
-        dhk = grad_hk.T @ dx_k
-        dmu_k = (tau_k - mu_k * dhk  )/hk  - mu_k
-        dmerit_k = grad_tot_k.T @ dx_k # this derivative is always negative
+        dx_k, dmu_k, dmerit_k = step_fn(x_k, mu_k, tau_k)
 
+        # Termination criterion
         if ca.norm_inf(dx_k) < opts["tol"]:
             stats["success"] = True
             break
+    
         # Line search to ensure feasibility and sufficient decrease
-        assert dmerit_k < 0, "Search direction is not a descent direction for the merit function."
-        merit_k = merit_fn(x_k)
+        assert dmerit_k < 0, f"Search direction is not a descent direction for the merit function: dmerit_k = {dmerit_k.full()[0,0]:2e}"
+        merit_k = merit_fn(x_k, tau_k)
         step_size = 1.0
         for i in range(opts["maxiter_line_search"]):
             x_ = x_k + step_size * dx_k
             mu_ = mu_k + step_size * dmu_k
             h_, sqrtP_ = cheap_h(x_)
             if sqrtP_.is_regular() and h_ >= 0 and mu_ >= 0:
-                merit_ = merit_fn(x_)
+                merit_ = merit_fn(x_, tau_k)
                 if merit_ < merit_k + opts["c_armijo"] * step_size * dmerit_k:
                     break
             step_size = opts["beta_line_search"] * step_size
